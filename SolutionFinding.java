48cc0517f707b969b8bae3e42e079b9b27007db7
Marin955 <marin.bilic55@gmail.com>
2019-04-29 10:29:02 +0200

    requires less memory space

--- SolutionFinding.java
+++ SolutionFinding.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 
 public class SolutionFinding {
     
-    Cube solvedState, initial, tempCube, tempCube2;
+    Cube solvedState, initial, tempCube;
     
     //String that stores the final solution
     String solution;
@@ -17,7 +17,7 @@ public class SolutionFinding {
     *   Storing all of the generated cube states into a HashSet because it doesn't allow duplicates
     *   and it's fast with adding, removing and checking whether the solution has been found.
     */
-    HashSet<Cube> set, tempSet;
+    HashSet<Cube> set, tempSet, tempSet2, oldSet;
     
     Iterator<Cube> iter, iter2;
     
@@ -30,6 +30,8 @@ public class SolutionFinding {
         set = new HashSet<>();
         set.add(initial);
         tempSet = new HashSet<>();
+        tempSet2 = new HashSet<>();
+        oldSet = new HashSet<>();
         iter = set.iterator();
     }
     
@@ -40,47 +42,35 @@ public class SolutionFinding {
     //recursive function that repeats until the solution is found
     public void findSolution() {
         if(!methodVisited && iter.next() == this.solvedState) this.solution = "Cube is already solved";
+        else if (!methodVisited) iter = set.iterator();
+        else if (methodVisited) iter = oldSet.iterator();
         methodVisited = true;
-        iter = set.iterator();
         
         //do all 9 moves on every cube state
         while(iter.hasNext()) {
             tempCube = iter.next();
+            tempSet.clear();
             
-            tempCube2 = operation.r(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.r2(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.r_p(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
+            tempSet.add(new Cube(operation.r(tempCube)));
+            tempSet.add(new Cube(operation.r2(tempCube)));
+            tempSet.add(new Cube(operation.r_p(tempCube)));
             
-            tempCube2 = operation.f(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.f2(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.f_p(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
+            tempSet.add(new Cube(operation.u(tempCube)));
+            tempSet.add(new Cube(operation.u2(tempCube)));
+            tempSet.add(new Cube(operation.u_p(tempCube)));
             
-            tempCube2 = operation.u(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.u2(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
-            tempCube2 = operation.u_p(tempCube);
-            tempSet.add(new Cube(tempCube2));
-            if(tempCube2.hashCode() == solvedState.hashCode()) break;
+            tempSet.add(new Cube(operation.f(tempCube)));
+            tempSet.add(new Cube(operation.f2(tempCube)));
+            tempSet.add(new Cube(operation.f_p(tempCube)));
+            
+            set.addAll(tempSet);
         }
         //add all cube states that differ from previously generated ones
-        set.addAll(tempSet);
-        
-        tempSet.clear();
+        tempSet2.clear();
+        tempSet2.addAll(set);
+        tempSet2.removeAll(oldSet);
+        oldSet.addAll(tempSet2);
+
         System.out.println("Current HashSet size: " + set.size());
         
         iter2 = set.iterator();
