100c5107bf3599e08b7966555910a7f2832f685a
Marin955 <marin.bilic55@gmail.com>
2019-05-02 08:08:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,21 @@ public class Main {
         CubeOperations oper = new CubeOperations();
         SolutionFinding solve;
         
+        //##################################################
+        // Input of whatever state we want to solve:
+        String stateToBeSolved = "ywborworbyrwogywrbgogbgy";
+        //##################################################
+        
+        String2Cube convertOperation4StateToBeSolved = new String2Cube(stateToBeSolved);
+        convertOperation4StateToBeSolved.convert();
+        Cube initial = new Cube(convertOperation4StateToBeSolved.getCube().piece_positions, convertOperation4StateToBeSolved.getCube().connectionsInfo, "");
+        initial.prev_moves = "Solution: ";
+        
         //creating a Cube object that is in a solved state
         String solved = "ggggyyyyrrrrwwwwoooobbbb";
         String2Cube convertOperation4SolvedState = new String2Cube(solved);
         convertOperation4SolvedState.convert();
         Cube solvedState = new Cube(convertOperation4SolvedState.getCube().piece_positions, convertOperation4SolvedState.getCube().connectionsInfo, "");
-        
-        //Input of whatever state we want solved
-        String stateToBeSolved = "ywborworbyrwogywrbgogbgy";
-        String2Cube convertOperation4StateToBeSolved = new String2Cube(stateToBeSolved);
-        convertOperation4StateToBeSolved.convert();
-        Cube initial = new Cube(convertOperation4StateToBeSolved.getCube().piece_positions, convertOperation4StateToBeSolved.getCube().connectionsInfo, "");
-        initial.prev_moves = "Solution: ";
         
         solve = new SolutionFinding(solvedState, initial);
         solve.findSolution();
